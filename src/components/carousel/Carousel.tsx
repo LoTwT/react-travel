a1f46a6f816cb5fd91cb06d4ed92020062015545
LoTwT <709937065@qq.com>
2022-01-12 20:55:36 +0800

    feat: sideMenu & carousel

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,16 @@
+import styles from "./Carousel.module.css"
+import { Image, Carousel as ACarousel } from "antd"
+
+import carouselImage1 from "../../assets/images/carousel_1.jpg"
+import carouselImage2 from "../../assets/images/carousel_2.jpg"
+import carouselImage3 from "../../assets/images/carousel_3.jpg"
+
+export const Carousel = () => {
+  return (
+    <ACarousel autoplay className={styles.slider}>
+      <Image src={carouselImage1} />
+      <Image src={carouselImage2} />
+      <Image src={carouselImage3} />
+    </ACarousel>
+  )
+}
