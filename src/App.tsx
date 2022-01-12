d5b4b34b29e33d32dcc9e7a920244e4526d7faa3
LoTwT <709937065@qq.com>
2022-01-12 17:45:54 +0800

    feat: init & header & footer

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css"
+
+import { Header, Footer } from "./components"
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
