71f672d14b90be68855f313e8e11a27c72dc7212
LoTwT <709937065@qq.com>
2022-01-13 13:35:07 +0800

    feat: add i18n

--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -1,3 +1,5 @@
+import i18n from "i18next"
+
 export interface LanguageState {
   language: "en" | "zh"
   languageList: { name: string; code: string }[]
@@ -14,6 +16,7 @@ const defaultState: LanguageState = {
 export default (state = defaultState, action: any) => {
   switch (action.type) {
     case "change_language":
+      i18n.changeLanguage(action.payload)
       return {
         ...state,
         language: action.payload,
