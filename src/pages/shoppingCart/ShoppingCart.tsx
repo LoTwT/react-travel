7f5d5d1020b47026c633ab1c36c3ee64cb15c1c2
LoTwT <709937065@qq.com>
2022-01-14 13:41:29 +0800

    feat: shoppingCart ui

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,3 +1,28 @@
+import styles from "./ShoppingCart.module.css"
+import { Row, Col, Affix } from "antd"
+import { MainLayout } from "../../layouts/mainLayout"
+import { ProductList, PaymentCard } from "../../components"
+
 export const ShoppingCart = () => {
-  return <div>购物车</div>
+  return (
+    <MainLayout>
+      <Row>
+        {/* 购物车清单 */}
+        <Col span={16}>
+          <div className={styles["product-list-container"]}>
+            {/* <ProductList /> */}
+          </div>
+        </Col>
+
+        {/* 支付卡组件 */}
+        <Col span={8}>
+          <Affix>
+            <div className={styles["payment-card-container"]}>
+              {/* <PaymentCard /> */}
+            </div>
+          </Affix>
+        </Col>
+      </Row>
+    </MainLayout>
+  )
 }
